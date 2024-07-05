7beddeb654ed54485a55160497430697e789ac04
Adesh Singh <adeshsingh3103@gmail.com>
2024-07-05 14:22:41 +0530

    enhanced features

--- src/components/DiscoverMovies.js
+++ src/components/DiscoverMovies.js
@@ -15,6 +15,13 @@ const TrendingMovies = () => {
       .then((data) => setMovies(data.results));
   }, []);
 
+  const truncateOverview = (text, limit) => {
+    if (text.length > limit) {
+      return text.slice(0, limit) + "...";
+    }
+    return text;
+  };
+
   return (
     <div className='container mx-auto my-8'>
       <h3 className='text-2xl font-bold text-indigo-600 mb-4'>
@@ -24,23 +31,41 @@ const TrendingMovies = () => {
         {movies
           .filter((e, index) => index <= 6)
           .map((movie) => (
-            <div key={movie.id} className='bg-white p-4 rounded-lg shadow-md'>
+            <div
+              key={movie.id}
+              className='relative p-4 rounded-lg shadow-md group'
+            >
               <img
                 src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
                 alt={movie.title}
                 className='w-full h-64 object-cover rounded-lg mb-2'
               />
-              <h4 className='text-gray-800 text-lg font-bold'>{movie.title}</h4>
+              <div className='absolute bottom-0 left-0 right-0 h-36 bg-gradient-to-t from-black to-transparent rounded-b-lg'></div>
+              <h4 className='absolute bottom-2 left-4 text-white text-lg font-bold'>
+                {movie.title}
+              </h4>
+              <div className='absolute inset-0 bg-black bg-opacity-75 flex items-center justify-center text-white text-center p-4 rounded-lg opacity-0 group-hover:opacity-100 transition-opacity'>
+                <div className='flex flex-col mb-4'>
+                  <div> {truncateOverview(movie.overview, 100)}</div>
+
+                  <Link
+                    to={`/movie/${movie.id}`}
+                    className='bg-gray-500 text-black-500 font-semibold py-3 px-6 rounded-full shadow-lg  hover:text-white transition duration-300 ease-in-out transform hover:scale-105 w-1/2 mx-auto mt-4 '
+                  >
+                    See More
+                  </Link>
+                </div>
+              </div>
             </div>
           ))}
         <div className='max-w-sm rounded overflow-hidden shadow-lg p-4 bg-white'>
           <div className='bg-gray-200 w-full h-full'>
-            <div className='flex items-center justify-center h-full bg-gradient-to-r from-blue-500 to-purple-500'>
+            <div className='flex items-center justify-center h-full bg-gradient-to-r from-black-200 to-black-300'>
               <Link
                 to={"/trending-movies"}
-                className='bg-gray-500 text-black-500 font-semibold py-3 px-6 rounded-full shadow-lg  hover:text-white transition duration-300 ease-in-out transform hover:scale-105'
+                className='bg-gray-500 text-black font-semibold py-3 px-6 rounded-full shadow-lg hover:text-white transition duration-300 ease-in-out transform hover:scale-105'
               >
-                See More
+                Explore More..
               </Link>
             </div>
           </div>
--- src/components/DiscoverShows.js
+++ src/components/DiscoverShows.js
@@ -4,6 +4,13 @@ import { Link } from "react-router-dom";
 const PopularShows = () => {
   const [shows, setShows] = useState([]);
 
+  const truncateOverview = (text, limit) => {
+    if (text.length > limit) {
+      return text.slice(0, limit) + "...";
+    }
+    return text;
+  };
+
   useEffect(() => {
     fetch("https://api.themoviedb.org/3/discover/tv", {
       headers: {
@@ -24,23 +31,41 @@ const PopularShows = () => {
         {shows
           .filter((e, index) => index <= 6)
           .map((show) => (
-            <div key={show.id} className='bg-white p-4 rounded-lg shadow-md'>
+            <div
+              key={show.id}
+              className='relative p-4 rounded-lg shadow-md group'
+            >
               <img
                 src={`https://image.tmdb.org/t/p/w500/${show.poster_path}`}
-                alt={show.name}
+                alt={show.title}
                 className='w-full h-64 object-cover rounded-lg mb-2'
               />
-              <h4 className='text-gray-800 text-lg font-bold'>{show.name}</h4>
+              <div className='absolute bottom-0 left-0 right-0 h-36 bg-gradient-to-t from-black to-transparent rounded-b-lg'></div>
+              <h4 className='absolute bottom-2 left-4 text-white text-lg font-bold'>
+                {show.title}
+              </h4>
+              <div className='absolute inset-0 bg-black bg-opacity-75 flex items-center justify-center text-white text-center p-4 rounded-lg opacity-0 group-hover:opacity-100 transition-opacity'>
+                <div className='flex flex-col mb-4'>
+                  <div> {truncateOverview(show.overview, 100)}</div>
+
+                  <Link
+                    to={`/tv-show/${show.id}`}
+                    className='bg-gray-500 text-black-500 font-semibold py-3 px-6 rounded-full shadow-lg  hover:text-white transition duration-300 ease-in-out transform hover:scale-105 w-1/2 mx-auto mt-4 '
+                  >
+                    See More
+                  </Link>
+                </div>
+              </div>
             </div>
           ))}
         <div className='max-w-sm rounded overflow-hidden shadow-lg p-4 bg-white'>
           <div className='bg-gray-200 w-full h-full'>
-            <div className='flex items-center justify-center h-full bg-gray-400 '>
+            <div className='flex items-center justify-center h-full bg-gradient-to-r from-black-200 to-black-300'>
               <Link
                 to={"/trending-shows"}
-                className='bg-gray-500 text-black-500 font-semibold py-3 px-6 rounded-full shadow-lg  hover:text-white transition duration-300 ease-in-out transform hover:scale-105'
+                className='bg-gray-500 text-black font-semibold py-3 px-6 rounded-full shadow-lg hover:text-white transition duration-300 ease-in-out transform hover:scale-105'
               >
-                See More
+                Explore More..
               </Link>
             </div>
           </div>
